b0fe94fd0f60267a1d1f1acfe09e3f95f700f20f
Henry Li <henry1943@163.com>
2018-09-15 14:04:43 +0800

    Add joystick

---
+++ src/Joystick.cpp
@@ -0,0 +1,65 @@
+#include "Joystick.h"
+
+const uint8_t MIN_CHANGE = 2;
+
+Joystick::Joystick(uint8_t xPin, uint8_t yPin) {
+  _xPin = xPin;
+  _yPin = yPin;
+}
+
+int Joystick::x() {
+  return _x;
+}
+
+int Joystick::y() {
+  return _y;
+}
+
+void Joystick::begin() {
+  _x = 0;
+  _y = 0;
+  _lastUpdateTime = 0;
+}
+
+void Joystick::update() {
+  if (millis() - _lastUpdateTime > _updateInterval) {
+    int x = analogRead(_xPin) - 512 - _offsetX;
+    int y = analogRead(_yPin) - 512 - _offsetY;
+
+    if (abs(x) <= MIN_CHANGE) {
+      x = 0;
+    }
+    if (abs(y) <= MIN_CHANGE) {
+      y = 0;
+    }
+
+    if (x > 511) {
+      x = 511;
+    }
+    if (x < -511) {
+      x = -511;
+    }
+
+    if (y > 511) {
+      y = 511;
+    }
+    if (y < -511) {
+      y = -511;
+    }
+
+    if (_x != x || _y != y) {
+      _x = x;
+      _y = y;
+      printDebug();
+    }
+
+    _lastUpdateTime = millis();
+  }
+}
+
+void Joystick::printDebug() {
+  Serial.print(_x);
+  Serial.print(", ");
+  Serial.print(_y);
+  Serial.println();
+}
---
+++ src/Joystick.h
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+class Joystick {
+public:
+  Joystick(uint8_t xPin, uint8_t yPin);
+
+  int x();
+  int y();
+
+  void begin();
+  void update();
+  void printDebug();
+
+private:
+  int _x = 0;
+  int _y = 0;
+
+  int8_t _offsetX = 1;
+  int8_t _offsetY = -5;
+
+  uint8_t _xPin = 0;
+  uint8_t _yPin = 0;
+
+  unsigned int _updateInterval = 50;
+  unsigned long _lastUpdateTime = 0;
+};
