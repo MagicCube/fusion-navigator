e2e5c405a73b0ed36d0192e96cd47b45f03ae8d7
亨睿 <henry.lx@alibaba-inc.com>
2018-09-18 16:41:33 +0800

    Add calibration

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "FusionNavigator.h"
 #include "Joystick.h"
 
-Joystick joystick(JOYSTICK_X_PIN, JOYSTICK_Y_PIN);
+Joystick joystick(JOYSTICK_X_PIN, JOYSTICK_Y_PIN, JOYSTICK_OFFSET_X, JOYSTICK_OFFSET_Y);
 Encoder encoder(ENCODER_CLS_PIN, ENCODER_DT_PIN);
 FusionNavigator fusionNavigator(&joystick, &encoder, ENCODER_SW_PIN, BUTTON_1_PIN);
 
--- src/settings.h
+++ src/settings.h
@@ -1,23 +1,17 @@
-// Joystick pins
+// Joystick
 #define JOYSTICK_X_PIN A0
 #define JOYSTICK_Y_PIN A1
+#define JOYSTICK_CALIBRATING false
+#define JOYSTICK_OFFSET_X 0
+#define JOYSTICK_OFFSET_Y 0
 
-// Encoder pins (interruptible only)
+// Encoder (interruptible pins only)
 #define ENCODER_CLS_PIN 2
 #define ENCODER_DT_PIN 3
 #define ENCODER_SW_PIN 4
 
 // User Customizable Button pins
 #define BUTTON_1_PIN 5
-
-// Each Joystick need re-adjust before use.
-// #define JOYSTICK_ADJUSTING true
-// #define JOYSTICK_ADJUSTMENT_X 0
-// #define JOYSTICK_ADJUSTMENT_Y 0
-
-#define JOYSTICK_ADJUSTING false
-#define JOYSTICK_ADJUSTMENT_X 0
-#define JOYSTICK_ADJUSTMENT_Y -5
 
 #define UPDATE_INTERVAL 80
 #define PAN_SPEED 42
